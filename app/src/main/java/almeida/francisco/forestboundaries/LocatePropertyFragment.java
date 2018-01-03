bb315f7501bacd6e1a08f86ba293b806d7b071c0
Francisco Pisco Almeida <fmpapt@gmail.com>
2018-01-03 16:01:21 +0000

    working on adding markers to new properties

--- app/src/main/java/almeida/francisco/forestboundaries/LocatePropertyFragment.java
+++ app/src/main/java/almeida/francisco/forestboundaries/LocatePropertyFragment.java
@@ -18,6 +18,11 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import almeida.francisco.forestboundaries.model.Property;
 import almeida.francisco.forestboundaries.service.PropertyService;
@@ -29,10 +34,16 @@ public class LocatePropertyFragment
     private TextView propertyName;
     private Button locateMarkerBtn;
     private Button locateLaterBtn;
+    private Button saveMarkerBtn;
     private Button saveBtn;
 
     private long propertyId;
     private GoogleMap map;
+
+    private Marker marker;
+    private List<LatLng> points = new ArrayList<>();
+    private Polyline polyline = null;
+    private Boolean isPolylineOnMap = false;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -61,15 +72,15 @@ public class LocatePropertyFragment
         propertyName = view.findViewById(R.id.prop_name_loc);
         locateMarkerBtn = view.findViewById(R.id.create_new_marker);
         locateLaterBtn = view.findViewById(R.id.locate_later);
+        saveMarkerBtn = view.findViewById(R.id.save_marker_locate);
         saveBtn = view.findViewById(R.id.save_locate);
 
         locateMarkerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 LatLng centerLatLng = map.getCameraPosition().target;
-                Marker marker = map.addMarker(new MarkerOptions()
+                marker = map.addMarker(new MarkerOptions()
                         .position(centerLatLng).draggable(true));
-
             }
         });
 
@@ -77,6 +88,28 @@ public class LocatePropertyFragment
             @Override
             public void onClick(View view) {
 
+            }
+        });
+
+        saveMarkerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                points.add(marker.getPosition());
+//                marker.remove();
+
+                if (points.size() > 1) {
+                    if (!isPolylineOnMap) {
+                        polyline = map.addPolyline(new PolylineOptions());
+                        isPolylineOnMap = true;
+                    }
+                    polyline.setPoints(points);
+                }
+
+                if (polyline == null) {
+                    PolylineOptions polylineOptions = new PolylineOptions()
+                            .add(marker.getPosition());
+                }
             }
         });
 
@@ -99,6 +132,15 @@ public class LocatePropertyFragment
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
+
+        map.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+            @Override
+            public void onMarkerDragStart(Marker marker) {}
+            @Override
+            public void onMarkerDrag(Marker marker) {}
+            @Override
+            public void onMarkerDragEnd(Marker marker) {}
+        });
     }
 
     public void setPropertyId(long propertyId) {
