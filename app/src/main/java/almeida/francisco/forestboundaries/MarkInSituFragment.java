6165480d7016bc9b6c3ecfc64c10b267caae2d8e
Francisco Pisco Almeida <fmpapt@gmail.com>
2018-01-08 17:28:40 +0000

    implementing gps location, getting null pointer when coming back from fragment

--- app/src/main/java/almeida/francisco/forestboundaries/MarkInSituFragment.java
+++ app/src/main/java/almeida/francisco/forestboundaries/MarkInSituFragment.java
@@ -1,8 +1,13 @@
 package almeida.francisco.forestboundaries;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -30,9 +35,16 @@ public class MarkInSituFragment extends Fragment implements OnMapReadyCallback {
 
     private final static String LOG_TAG = MarkInSituFragment.class.getName();
 
+    private final static int MIN_TIME_INTERVAL_IN_MS = 1000;
+    private final static int MIN_DIST_IN_METERS = 1;
+
+    public final static int MY_PERMISSIONS_REQUEST_LOCATION = 1;
+
     private long propertyId;
     private TextView propertyNameTxt;
     private Property property;
+    private String prefLocationProvider;
+    private LocationManager locMgr;
 
     public MarkInSituFragment() {}
 
@@ -55,7 +67,18 @@ public class MarkInSituFragment extends Fragment implements OnMapReadyCallback {
                 .commit();
         ((SupportMapFragment) mapFragment).getMapAsync(this);
 
-        LocationManager locMgr = (LocationManager) getActivity()
+        if (Build.VERSION.SDK_INT >= 23 &&
+                getActivity().checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED &&
+                getActivity().checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            getActivity().requestPermissions(
+                    new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_LOCATION);
+        }
+
+        locMgr = (LocationManager) getActivity()
                 .getSystemService(Context.LOCATION_SERVICE);
         for (String prov : locMgr.getAllProviders()) {
             Log.i(LOG_TAG, "Provider: " + prov);
@@ -67,9 +90,9 @@ public class MarkInSituFragment extends Fragment implements OnMapReadyCallback {
             Log.e(LOG_TAG, "cannot open GPS service.");
             Toast.makeText(getActivity(), "Could not open GPS service.", Toast.LENGTH_LONG)
                     .show();
+        } else {
+            prefLocationProvider = providers.get(0);
         }
-        String prefered = providers.get(0);
-
 
         return inflater.inflate(R.layout.fragment_mark_in_situ, container, false);
     }
@@ -85,6 +108,42 @@ public class MarkInSituFragment extends Fragment implements OnMapReadyCallback {
         PropertyService propertyService = new PropertyService(getActivity());
         property = propertyService.findById(propertyId);
         propertyNameTxt.setText(property.getLocationAndDescription());
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (prefLocationProvider != null && locMgr != null) {
+            try {
+                locMgr.requestLocationUpdates(prefLocationProvider,
+                        MIN_TIME_INTERVAL_IN_MS,
+                        MIN_DIST_IN_METERS, new LocationListener() {
+                            @Override
+                            public void onLocationChanged(Location location) {
+                                String lat = Double.toString(location.getLatitude());
+                                String lon = Double.toString(location.getLongitude());
+                                Toast
+                                        .makeText(
+                                        getActivity(), lat + lon, Toast
+                                                        .LENGTH_SHORT)
+                                        .show();
+                            }
+
+                            @Override
+                            public void onStatusChanged(String s, int i, Bundle bundle) {}
+
+                            @Override
+                            public void onProviderEnabled(String s) {}
+
+                            @Override
+                            public void onProviderDisabled(String s) {}
+                        });
+            } catch (SecurityException e) {
+                e.printStackTrace();
+            }
+
+        }
+
     }
 
     public void setPropertyId(long propertyId) {
